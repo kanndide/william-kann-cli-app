2362825f23f9bdcd45f62491bdabdb4d744658ac
kanndide <kanndide@gmail.com>
2018-02-03 13:29:41 +0300

    working on getting scraper to pull correct information

--- lib/william_kann_cli_app/scraper.rb
+++ lib/william_kann_cli_app/scraper.rb
@@ -8,21 +8,27 @@ class EdgarScraper
     @@doc = Nokogiri::HTML(open(@@index_url))
     
     def self.scrape_years
-        index_url = "https://www.sec.gov/Archives/edgar/full-index/"
-        doc = Nokogiri::HTML(open(index_url))
-        years = doc.css("td a").collect {|x| x.attribute("href").value}
+        years = @@doc.css("td a").collect {|x| x.attribute("href").value}
         years -= years.select {|x| x.include?("xml")}     
     end
     
-    def self.scrape_for_last_13fhr(year)
-        index_url = "https://www.sec.gov/Archives/edgar/full-index/#{year}/"
-        doc = Nokogiri::HTML(open(index_url))
-        qtrs = doc.css("td a").collect {|x| x.attribute("href").value}
-        index_url << qtrs.last
-        doc = Nokogiri::HTML(open(index_url))
-        files_to_scrape = 
+    def self.scrape_for_qtr(year)
+        @@index_url << "#{year}/"
+        @@doc = Nokogiri::HTML(open(@@index_url))
+        qtrs = @@doc.css("td a").collect {|x| x.attribute("href").value}
+        binding.pry
+    end
+    
+    def self.scrape_for_last_13fhr(qtr)
+        @@index_url << "#{qtr.upcase}/"
+        @@doc = Nokogiri::HTML(open(@@index_url))
+        files_to_scrape = @@doc.css("td a").select {
+            |x| x.attribute("href").value.include?(".xml")}.collect {
+                |x| x.attribute("href").value}
+        binding.pry
     end
     
 end
 
-EdgarScraper.scrape_for_last_13fhr("2017/")
+EdgarScraper.scrape_for_qtr("2017")
+EdgarScraper.scrape_for_last_13fhr("qtr4")

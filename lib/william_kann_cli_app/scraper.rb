3f448d333810304b3744a1e5e4bd41c02bfb91b6
kanndide <kanndide@gmail.com>
2018-02-04 16:37:47 +0300

    flushing out scraper methods

--- lib/william_kann_cli_app/scraper.rb
+++ lib/william_kann_cli_app/scraper.rb
@@ -4,8 +4,8 @@ require 'pry'
 
 class EdgarScraper
     
-    @@index_url = "https://www.sec.gov/Archives/edgar/full-index/"
-    @@doc = Nokogiri::HTML(open(@@index_url))
+    INDEX_URL = "https://www.sec.gov/Archives/edgar/full-index/"
+    @@doc = Nokogiri::HTML(open(INDEX_URL))
     
     def self.scrape_years
         years = @@doc.css("td a").collect {|x| x.attribute("href").value}
@@ -13,22 +13,24 @@ class EdgarScraper
     end
     
     def self.scrape_for_qtr(year)
-        @@index_url << "#{year}/"
-        @@doc = Nokogiri::HTML(open(@@index_url))
-        qtrs = @@doc.css("td a").collect {|x| x.attribute("href").value}
-        binding.pry
+        index_url = INDEX_URL.dup
+        index_url << "#{year}/"
+        doc = Nokogiri::HTML(open(index_url))
+        qtrs = doc.css("td a").collect {|x| x.attribute("href").value}
     end
     
-    def self.scrape_for_last_13fhr(qtr)
-        @@index_url << "#{qtr.upcase}/"
-        @@doc = Nokogiri::HTML(open(@@index_url))
-        files_to_scrape = @@doc.css("td a").select {
+    
+    def self.scrape_for_13fhr(year, qtr)
+        index_url = INDEX_URL.dup
+        index_url << "#{year}/#{qtr.upcase}/"
+        doc = Nokogiri::HTML(open(index_url))
+        
+        files_to_scrape = doc.css("td a").select {
             |x| x.attribute("href").value.include?(".xml")}.collect {
                 |x| x.attribute("href").value}
-        binding.pry
+        
     end
     
 end
 
-EdgarScraper.scrape_for_qtr("2017")
-EdgarScraper.scrape_for_last_13fhr("qtr4")
+EdgarScraper.scrape_for_13fhr("2017", "qtr4")

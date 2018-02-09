e0102a03d52e2450026c89e1f2de1eafa7adfebb
kanndide <kanndide@gmail.com>
2018-02-09 06:46:45 +0300

    stubbing out some CLI shit

--- lib/william_kann_cli_app/cli.rb
+++ lib/william_kann_cli_app/cli.rb
@@ -6,14 +6,27 @@ class WilliamKannCliApp::CLI
     
     def main_menu
         puts "What would you like to see?"
-        puts "Current holdings, or holdings for another year?"
+        puts "Current holdings, or holdings for another year? Type 'current holdings' or 'different year'."
         user_input = gets.strip
+        if user_input == "Current holdings" || user_input == "current holdings" || user_input == "Current Holdings"
+            EdgarScraper.hash_13fhr("2017", "qtr4")
+        elsif user_input == "Different year" || user_input == "different year" || user_input == "Different year"
+            choose_your_own
+        end
     end
     
     def display_years
         EdgarScraper.scrape_years.each do |x|
             
         end
+    end
+    
+    def display_qtr
+        
+    end
+    
+    def choose_your_own
+        
     end
     
     
